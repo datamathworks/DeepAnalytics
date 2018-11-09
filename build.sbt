cbcb015cbd6fda1bba98efd6b333e764b9b7c1c1
Dorina Stratulat <dorinastratulat@Dorinas-MacBook-Pro-2.local>
2018-11-09 14:56:52 -0500

    word count app - initial commit

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "DeepAnalytics"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+val sparkVersion = "2.4.0"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test
+)
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,72 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+  def main(args: Array[String]) : Unit = {
+      val conf = new SparkConf().setAppName("Word Count").setMaster("local")
+      val sc = new SparkContext(conf);
+
+      val inputFile = sc.textFile("data/macbeth.txt")
+                        /* sample output:
+                            Actus
+                            Primus.
+                            Scoena
+                            Prima. */
+                        .flatMap(_.split(" "))
+                        /* sample output:
+                            actus
+                            primus
+                            scoena
+                            prima  */
+                        .map(_.replaceAll("[,.!?:;]", "")
+                              .trim
+                              .toLowerCase)
+                        .filter(!_.isEmpty)
+
+                        /* sample output:
+                            (actus,1)
+                            (primus,1)
+                            (scoena,1)
+                            (prima,1)
+                            (thunder,1) */
+                       .map(word => (word, 1))
+
+                        /* sample output:
+                            (masking,1)
+                            (fantasticall,2)
+                            (battlements,2)
+                            (doo't,2)
+                            (oppos'd,1) */
+                       .reduceByKey(_ + _)
+                        /* sample output:
+                            (1,masking)
+                            (2,fantasticall)
+                            (2,battlements)
+                            (2,doo't)
+                            (1,oppos'd)*/
+                       .map{case(word, count) => (count,word)}
+                       /* sample output:
+                           (644,the)
+                           (544,and)
+                           (383,to)
+                           (336,of)
+                           (330,i)*/
+                       .sortByKey(ascending = false)
+                      /* sample output:
+                           (the,644)
+                           (and,544)
+                           (to,383)
+                           (of,336)
+                           (i,330)*/
+                       .map{case(count,word) => (word,count)}
+                      /* sample output:
+                          (the,644,3)
+                          (and,544,3)
+                          (to,383,2)
+                          (of,336,2)
+                          (i,330,1)*/
+                       .map{case(word,count) => (word,count,word.length)}
+                       .collect() take 20 foreach println;
+
+
+  }
+}
